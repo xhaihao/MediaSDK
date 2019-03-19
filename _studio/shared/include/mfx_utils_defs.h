6cf73561c494822c5c836be4dc6f59a9123a71b6
Haihao Xiang <haihao.xiang@intel.com>
2019-03-19 12:43:18 +0800

    Don't call function twice

--- _studio/shared/include/mfx_utils_defs.h
+++ _studio/shared/include/mfx_utils_defs.h
@@ -60,8 +60,7 @@ static inline T mfx_print_err(T sts, const char *file, int line, const char *fun
 
 #define MFX_CHECK_UMC_ALLOC(err)     { if (err != true) {return MFX_ERR_MEMORY_ALLOC;} }
 #define MFX_CHECK_EXBUF_INDEX(index) { if (index == -1) {return MFX_ERR_MEMORY_ALLOC;} }
-
-#define MFX_CHECK_WITH_ASSERT(EXPR, ERR) {assert(EXPR); MFX_CHECK(EXPR,ERR); }
+#define MFX_CHECK_WITH_ASSERT(EXPR, ERR) { bool _ret = !!(EXPR); assert(_ret); MFX_CHECK(_ret, ERR); }
 #define MFX_CHECK_WITH_THROW(EXPR, ERR, EXP)  { if (!(EXPR)) { std::ignore = MFX_STS_TRACE(ERR); throw EXP; } }
 
 static const mfxU32 NO_INDEX = 0xffffffff;
